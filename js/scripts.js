abe9296050bf378605d1fd30159760893500d333
Nathan Sanchez <nathan1220sanchez@gmail.com>
2023-08-13 19:08:25 -0400

    ..

--- js/scripts.js
+++ js/scripts.js
@@ -14,11 +14,11 @@ takes an array of words and smashes
 smash = function (words) {
     return words.join(" ");
   };
-  
+
   //Test code
-const chai = require("chai");
-const assert = chai.assert;
-chai.config.truncateThreshold=0;
+// const chai = require("chai");
+// const assert = chai.assert;
+// chai.config.truncateThreshold=0;
 
 describe("smash", () =>  {
 
@@ -34,5 +34,32 @@ describe("smash", () =>  {
     assert.strictEqual(smash(["hello", "world"]), "hello world");
     assert.strictEqual(smash(["hello", "amazing", "world"]), "hello amazing world");
     assert.strictEqual(smash(["this", "is", "a", "really", "long", "sentence"]), "this is a really long sentence");
+  });
+});
+/*oposites attract: 
+Timmy & Sarah think they are in love, but around 
+where they live, they will only know once they pick a flower each. 
+If one of the flowers has an even number of petals and the other has an odd number of petals it means they are in love.
+
+Write a function that will take the number of petals of each flower and return true if they are in love and false if they aren't. */
+function lovefunc(flower1, flower2) {
+    // Check if one flower has even petals and the other has odd petals
+    if ((flower1 % 2 === 0 && flower2 % 2 !== 0) || (flower1 % 2 !== 0 && flower2 % 2 === 0)) {
+      return true; // They are in love
+    } else {
+      return false; // They are not in love
+    }
+  }
+
+  const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(lovefunc(1,4), true)
+    assert.strictEqual(lovefunc(2,2), false)
+    assert.strictEqual(lovefunc(0,1), true)
+    assert.strictEqual(lovefunc(0,0), false)
   });
 });
