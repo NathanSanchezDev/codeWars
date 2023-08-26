3770ed3c5e3bb6d9a46bb064566ae29e42cfa22b
Nathan Sanchez <NathanSanchezDev@outlook.com>
2023-08-26 14:08:52 -0400

    added codewars kata

--- js/scripts.js
+++ js/scripts.js
@@ -109,3 +109,20 @@ function solution(str, ending) {
 // Test cases
 console.log(solution('abc', 'bc'));  // returns true
 console.log(solution('abc', 'd'));   // returns false
+
+// We need a function that can transform a number (integer) into a string.
+// What ways of achieving this do you know?
+// Examples (input --> output):
+// 123  --> "123"
+// 999  --> "999"
+// -100 --> "-100"
+function numberToString(num) {
+  return num.toString();
+}
+//test code
+const assert = require('chai').assert;
+describe("Tests", () => {
+  it("test", () => {
+    assert.strictEqual(numberToString(67), '67');
+  });
+});
