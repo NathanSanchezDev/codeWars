00445768f48333125f29b927d5719fc6cdf8d9fd
Nathan Sanchez <NathanSanchezDev@outlook.com>
2023-08-26 14:25:37 -0400

    square individual numbers in a function & concat back together.

--- js/scripts.js
+++ js/scripts.js
@@ -126,3 +126,48 @@ describe("Tests", () => {
     assert.strictEqual(numberToString(67), '67');
   });
 });
+// square every digit of a number and concatenate them.
+
+// For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1. (81-1-1-81)
+
+// Example #2: An input of 765 will/should return 493625 because 72 is 49, 62 is 36, and 52 is 25. (49-36-25)
+
+// Note: The function accepts an integer and returns an integer.
+function squareDigits(num){
+  //we check if num is 0
+    if(num === 0){
+        return 0;
+    }
+  
+  //we define the result variable
+  let result = ' '
+  //create an iteration that will square the number
+  while (num > 0){
+    //define the whole digit
+    const digit = num % 10
+    //square the whole digit
+    const squaredDigit = digit ** 2
+    //formula to concat together
+    result = squaredDigit + result
+    //round to the nearest 10th
+    num = Math.floor(num / 10)
+  }
+    return parseInt(result)
+}
+//test code
+const { assert } = require("chai")
+
+describe("Basic tests", () => {
+  
+  it("squareDigits(3212) should equal 9414", () => {
+    assert.strictEqual(squareDigits(3212), 9414);
+  });
+
+  it("squareDigits(2112) should equal 4114", () => {
+    assert.strictEqual(squareDigits(2112), 4114);
+  });
+
+  it("squareDigits(0) should equal 0", () => {
+    assert.strictEqual(squareDigits(0), 0);
+  });
+})
