52f50d2e0923b565d90ec6183668687c267302b0
Nathan Sanchez <NathanSanchezDev@outlook.com>
2023-08-28 23:17:57 -0400

    negative number function solution

--- js/scripts.js
+++ js/scripts.js
@@ -214,3 +214,32 @@ Test.assertEquals(sum([1, 5.2, 4, 0, -1]), 9.2);
   });
 });
 
+
+//problem: 
+//In this simple assignment you are given a number and have to make it negative. But maybe the number is already negative?
+
+// Examples
+// makeNegative(1);    // return -1
+// makeNegative(-5);   // return -5
+// makeNegative(0);    // return 0
+// makeNegative(0.12); // return -0.12
+// Notes
+// The number can be negative already, in which case no change is required.
+// Zero (0) is not checked for any specific sign. Negative zeros make no mathematical sense.
+//code
+function makeNegative(num) {
+  if (num > 0) {
+    return -num;
+  } else {
+    return num;
+  }
+}
+
+//test code
+const Test = require('@codewars/test-compat');
+
+describe("Tests", () => {
+  it("test", () => {
+Test.assertEquals(makeNegative(42), -42);
+  });
+});
