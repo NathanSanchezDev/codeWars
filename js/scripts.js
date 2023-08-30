1b727b96a1936760dcf6503186e0c4d422bb0d12
Nathan Sanchez <NathanSanchezDev@outlook.com>
2023-08-29 23:35:49 -0400

    triangle formula function solution

--- js/scripts.js
+++ js/scripts.js
@@ -243,3 +243,22 @@ describe("Tests", () => {
 Test.assertEquals(makeNegative(42), -42);
   });
 });
+
+//Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+
+//(In this case, all triangles must have surface greater than 0 to be accepted).
+function isTriangle(a,b,c){
+  if (a + b > c && a + c > b && b + c > a){
+    return true
+  } else{
+  return false;}
+  }
+  //test
+  const { assert } = require("chai")
+
+describe("Public tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(isTriangle(1,2,2), true);
+    assert.strictEqual(isTriangle(7,2,2), false);
+  });
+})
