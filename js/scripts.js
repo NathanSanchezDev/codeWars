be79b09a273a857ce38ca2475a2addc580a0fa4c
Nathan Sanchez <NathanSanchezDev@outlook.com>
2023-08-30 23:23:00 -0400

    numberphobia on specific days coding solution

--- js/scripts.js
+++ js/scripts.js
@@ -288,3 +288,43 @@ Test.assertEquals(chromosomeCheck('XY'), "Congratulations! You're going to have 
 Test.assertEquals(chromosomeCheck('XX'), "Congratulations! You're going to have a daughter.")
   });
 });
+/*I've got a crazy mental illness. I dislike numbers a lot. But it's a little complicated: The number I'm afraid of depends on which day of the week it is... This is a concrete description of my mental illness:
+
+Monday --> 12
+
+Tuesday --> numbers greater than 95
+
+Wednesday --> 34
+
+Thursday --> 0
+
+Friday --> numbers divisible by 2
+
+Saturday --> 56
+
+Sunday --> 666 or -666
+
+Write a function which takes a string (day of the week) and an integer (number to be tested) so it tells the doctor if I'm afraid or not. (return a boolean)
+*/
+var AmIAfraid = function(day, num) {
+  if ((day === 'Monday' && num === 12) ||
+      (day === 'Tuesday' && num > 95) ||
+      (day === 'Wednesday' && num === 34) ||
+      (day === 'Thursday' && num === 0) ||
+      (day === 'Friday' && num % 2 === 0) ||
+      (day === 'Saturday' && num === 56) ||
+      (day === 'Sunday' && (num === 666 || num === -666))) {
+      return true;
+  } else {
+      return false;
+  }
+}
+
+//test
+describe('Fixed Tests', () => {
+  Test.assertEquals(AmIAfraid("Monday", 13), false, 'Should return false');
+  Test.assertEquals(AmIAfraid("Sunday", -666), true, 'Should return true');
+  Test.assertEquals(AmIAfraid("Tuesday", 2), false, 'Should return false');
+  Test.assertEquals(AmIAfraid("Tuesday", 965), true, 'Should return true');
+  Test.assertEquals(AmIAfraid("Friday", 2), true, 'Should return true');
+});
