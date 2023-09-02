2718318c70fe6d92eb8df244938a35051db5b5d0
Nathan Sanchez <NathanSanchezDev@outlook.com>
2023-09-02 19:06:50 -0400

    added coding solution

--- js/scripts.js
+++ js/scripts.js
@@ -352,3 +352,60 @@ console.log(sumAll(1, 4)); // Output: 10 (1 + 2 + 3 + 4 = 10)
 console.log(sumAll(4, 1)); // Output: 10 (1 + 2 + 3 + 4 = 10)
 console.log(sumAll(-3, 2)); // Output: -3 (-3 + -2 + -1 + 0 + 1 + 2 = -3)
 console.log(sumAll(5, 5)); // Output: 5 (a and b are equal)
+
+
+/*
+
+Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
+
+Don't change the order of the elements that are left.
+*/
+function removeSmallest(arr) {
+  if (arr.length === 0) {
+    return [];
+  }
+  let minValue = arr[0];
+  let minIndex = 0;
+
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < minValue) {
+      minValue = arr[i];
+      minIndex = i;
+    }
+  }
+  const newArr = [...arr.slice(0, minIndex), ...arr.slice(minIndex + 1)];
+  
+  return newArr;
+}
+//test samples
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("removeSmallest", function() {
+  it("works for the examples", function() {
+    assert.deepEqual(removeSmallest([1, 2, 3, 4, 5]), [2, 3, 4, 5], "Wrong result for [1, 2, 3, 4, 5]");
+    assert.deepEqual(removeSmallest([5, 3, 2, 1, 4]), [5, 3, 2, 4], "Wrong result for [5, 3, 2, 1, 4]");
+    assert.deepEqual(removeSmallest([2, 2, 1, 2, 1]), [2, 2, 2, 1], "Wrong result for [2, 2, 1, 2, 1]");
+    assert.deepEqual(removeSmallest([]), [], "Wrong result for []");
+  });
+  
+  it("returns [] if the list has only one element", function() {
+    for (let i = 0; i < 10; ++i) {
+      let x = ~~(Math.random() * 400);
+      assert.deepEqual(removeSmallest([x]), [], `Wrong result for ${[x]}`);
+    }
+  });
+  
+  function randomArray(length) {
+    return Array.from({length: length}, () => ~~(Math.random() * 400));
+  }
+  
+  it("returns a list that misses only one element", function() {
+    for(let i = 0; i < 10; ++i) {
+      let arr = randomArray(~~(Math.random() * 10) + 1);
+      let l = arr.length;
+      assert.strictEqual(removeSmallest(arr).length, l - 1, `Wrong result for ${arr}`);
+    }
+  });
+});
