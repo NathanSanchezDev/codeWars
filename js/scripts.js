45d7c0791a507f18ec58f754b73c600b215a174c
Nathan Sanchez <NathanSanchezDev@outlook.com>
2023-09-07 23:29:26 -0400

    return string with repeated chars. Solution

--- js/scripts.js
+++ js/scripts.js
@@ -492,3 +492,32 @@ Test.assertEquals(str.toJadenCase(), "How Can Mirrors Be Real If Our Eyes Aren't
 
   });
 });
+
+//Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+// Examples (Input -> Output):
+// * "String"      -> "SSttrriinngg"
+// * "Hello World" -> "HHeelllloo  WWoorrlldd"
+// * "1234!_ "     -> "11223344!!__  "
+function doubleChar(str) {
+  // Your code here
+ let result = '';
+  for (let i = 0; i < str.length; i++) {
+    result += str.charAt(i) + str.charAt(i);
+  }
+  return result;
+}
+
+//test
+const Test = require('@codewars/test-compat');
+
+describe("doubleChar", function() {
+  it("works for some examples", function() {
+    Test.assertEquals(doubleChar("abcd"), "aabbccdd");
+    Test.assertEquals(doubleChar("Adidas"), "AAddiiddaass");
+    Test.assertEquals(doubleChar("1337"), "11333377");
+    Test.assertEquals(doubleChar("illuminati"), "iilllluummiinnaattii");
+    Test.assertEquals(doubleChar("123456"), "112233445566");
+    Test.assertEquals(doubleChar("%^&*("), "%%^^&&**((");
+  });
+});
