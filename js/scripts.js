58a4f9a4d5835e91e7c4b1eab8c5d6f147371732
Nathan Sanchez <NathanSanchezDev@outlook.com>
2023-09-10 10:31:45 -0400

    Coding solution for returning the likes of people's name stored in an array

--- js/scripts.js
+++ js/scripts.js
@@ -548,3 +548,40 @@ console.log(result1); // Output: 7
 const array2 = [10, 343445353, 3453445, 3453545353453];
 const result2 = sumTwoSmallestNumbers(array2);
 console.log(result2); // Output: 3453455
+
+
+//question:
+// You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+
+// Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+
+// []                                -->  "no one likes this"
+// ["Peter"]                         -->  "Peter likes this"
+// ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+// ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+// ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+// Note: For 4 or more names, the number in "and 2 others" simply increases.
+
+
+//solution:
+function likes(names) {
+  const count = names.length;
+  switch (count) {
+    case 0:
+      return "no one likes this";
+    case 1:
+      return `${names[0]} likes this`;
+    case 2:
+      return `${names[0]} and ${names[1]} like this`;
+    case 3:
+      return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+    default:
+      return `${names[0]}, ${names[1]} and ${count - 2} others like this`;
+  }
+}
+// Test cases
+console.log(likes([]));                               // "no one likes this"
+console.log(likes(["Peter"]));                         // "Peter likes this"
+console.log(likes(["Jacob", "Alex"]));                 // "Jacob and Alex like this"
+console.log(likes(["Max", "John", "Mark"]));           // "Max, John and Mark like this"
+console.log(likes(["Alex", "Jacob", "Mark", "Max"]));  // "Alex, Jacob and 2 others like this"
