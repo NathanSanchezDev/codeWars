6b82b62ef9a5b2f160ff75a27ed893a2fcbdf4e7
Nathan Sanchez <NathanSanchezDev@outlook.com>
2023-09-20 12:46:48 -0400

    kata 7: Convert a binary array to an integer.

--- js/kata8-7-6.js
+++ js/kata8-7-6.js
@@ -525,7 +525,7 @@ describe("doubleChar", function() {
 
 //soluition:
 function positiveSum(arr) {
-  arr > 0 ? const sum = arr.reduce((a, b) => a + b); : return 
+  arr > 0 ? let sum2 = arr.reduce((a, b) => a + b); : return 
 }
 
 
@@ -585,3 +585,25 @@ console.log(likes(["Peter"]));                         // "Peter likes this"
 console.log(likes(["Jacob", "Alex"]));                 // "Jacob and Alex like this"
 console.log(likes(["Max", "John", "Mark"]));           // "Max, John and Mark like this"
 console.log(likes(["Alex", "Jacob", "Mark", "Max"]));  // "Alex, Jacob and 2 others like this"
+
+// Given an array of ones and zeroes, convert the equivalent binary value to an integer.
+
+// Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
+
+// Examples:
+
+// Testing: [0, 0, 0, 1] ==> 1
+// Testing: [0, 0, 1, 0] ==> 2
+// Testing: [0, 1, 0, 1] ==> 5
+// Testing: [1, 0, 0, 1] ==> 9
+// Testing: [0, 0, 1, 0] ==> 2
+// Testing: [0, 1, 1, 0] ==> 6
+// Testing: [1, 1, 1, 1] ==> 15
+// Testing: [1, 0, 1, 1] ==> 11
+// However, the arrays can have varying lengths, not just limited to 4.
+
+//My Solution:
+function binaryArrayToNumber(arr) {
+  const binaryString = arr.join('');
+  return parseInt(binaryString, 2);
+}
