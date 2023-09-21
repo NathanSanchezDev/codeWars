10fbad48ab826e875e2dbdcdd687db4fc6394522
Nathan Sanchez <NathanSanchezDev@outlook.com>
2023-09-21 17:19:52 -0400

    Kata 7 Solution for min max function for an array.

--- js/kata8-7-6.js
+++ js/kata8-7-6.js
@@ -607,3 +607,23 @@ function binaryArrayToNumber(arr) {
   const binaryString = arr.join('');
   return parseInt(binaryString, 2);
 }
+//kata7
+/*Story
+Ben has a very simple idea to make some profit: he buys something and sells it again. Of course, this wouldn't give him any profit at all if he was simply to buy and sell it at the same price. Instead, he's going to buy it for the lowest possible price and sell it at the highest.
+
+Task
+Write a function that returns both the minimum and maximum number of the given list/array.
+
+Examples (Input --> Output)
+[1,2,3,4,5] --> [1,5]
+[2334454,5] --> [5,2334454]
+[1]         --> [1,1]
+Remarks
+All arrays or lists will always have at least one element, so you don't need to check the length. Also, your function will always get an array or a list, you don't have to check for null, undefined or similar.
+*/
+//Solution:
+function minMax(arr) {
+  const minValue = Math.min(...arr);
+  const maxValue = Math.max(...arr);
+  return [minValue, maxValue];
+}
