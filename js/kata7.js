1a3c8e769b039f08c19c4c04581c870d14469c4d
Nathan Sanchez <NathanSanchezDev@outlook.com>
2023-09-30 15:25:17 -0400

    kata 7 pin validation solution

--- js/kata7.js
+++ js/kata7.js
@@ -19,3 +19,26 @@ function addLineNumbers(lines) {
     }
     return numberedLines;
   }
+
+//   //
+//   ATM machines allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+
+// If the function is passed a valid PIN string, return true, else return false.
+
+// Examples (Input --> Output)
+// "1234"   -->  true
+// "12345"  -->  false
+// "a234"   -->  false
+
+let solution = 
+function validatePIN(pin) {
+  if (pin.length !== 4 && pin.length !== 6) {
+    return false;
+  }
+  for (let i = 0; i < pin.length; i++) {
+    if (isNaN(parseInt(pin[i]))) {
+      return false;
+    }
+  }
+  return true;
+}
