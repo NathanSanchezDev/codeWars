3459abc914aa87acae061ade14987a7bf723da09
Nathan Sanchez <NathanSanchezDev@outlook.com>
2023-10-02 09:26:46 -0400

    Kata 6 Coding solution to sort numerical value in strings. Code solution provieded w/ code explaination

--- js/kata6.js
+++ js/kata6.js
@@ -136,3 +136,46 @@ findNb(91716553919377) --> -1
      return -1;
    }
    
+   /*
+   Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+
+Examples
+"is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+"4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+""  -->  ""
+*/
+//Solution: 
+const order = (words) => {
+  if (words === '') {
+    return '';
+  }
+  const wordArray = words.split(' ');
+  wordArray.sort((a, b) => {
+    const numA = extractNumber(a);
+    const numB = extractNumber(b);
+    return numA - numB;
+  });
+  return wordArray.join(' ');
+};
+const extractNumber = (word) => {
+  for (let i = 0; i < word.length; i++) {
+    if (!isNaN(word[i])) {
+      return parseInt(word[i]);
+    }
+  }
+  return -1;
+};
+/*
+Code explaination:
+We define a extractNumber function that iterates through each
+character of a word to find the first numeric character (digit).
+It then converts that character to an integer and returns it.
+If no number is found, it returns -1 (you can handle this differently as needed).
+
+Within the order function, we call extractNumber
+ to extract the number from each word for comparison during sorting.
+*/
