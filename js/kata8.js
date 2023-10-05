f8593218792c83889c2458d3d8e5b00548b73a80
Nathan Sanchez <NathanSanchezDev@outlook.com>
2023-10-04 22:43:33 -0400

    Kata 8 Power of 2 Coding Solution

--- js/kata8.js
+++ js/kata8.js
@@ -145,3 +145,20 @@ string.replaceAll('5', 'S').replaceAll('0', 'O').replaceAll('1', 'I');
 
 //Solution: 
 const testEven = (n) => n % 2 === 0;
+
+/*
+Complete the function that takes a non-negative integer n as input, and returns a list of all the powers of 2 with the exponent ranging from 0 to n ( inclusive ).
+
+Examples
+n = 0  ==> [1]        # [2^0]
+n = 1  ==> [1, 2]     # [2^0, 2^1]
+n = 2  ==> [1, 2, 4]  # [2^0, 2^1, 2^2]
+*/
+//Solution:
+function powersOfTwo(n) {
+  const result = [];
+  for (let i = 0; i <= n; i++) {
+    result.push(Math.pow(2, i));
+  }
+  return result;
+}
