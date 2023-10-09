6d5d8cf73e7b9d30e533d9e325fb7a33eea39efe
Nathan Sanchez <NathanSanchezDev@outlook.com>
2023-10-09 12:59:09 -0400

    Kata 6 & Kata 8 coding solutions added.

--- js/kata6.js
+++ js/kata6.js
@@ -179,3 +179,25 @@ If no number is found, it returns -1 (you can handle this differently as needed)
 Within the order function, we call extractNumber
  to extract the number from each word for comparison during sorting.
 */
+
+/*
+Task
+In this simple Kata your task is to create a function that turns a string into a Mexican Wave. You will be passed a string and you must return that string in an array where an uppercase letter is a person standing up. 
+Rules
+ 1.  The input string will always be lower case but maybe empty.
+
+ 2.  If the character in the string is whitespace then pass over it as if it was an empty seat
+Example
+wave("hello") => ["Hello", "hEllo", "heLlo", "helLo", "hellO"]
+*/
+//Solution:
+function wave(str) {
+  const waveArray = [];
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== ' ') {
+      const waveString = str.slice(0, i) + str[i].toUpperCase() + str.slice(i + 1);
+      waveArray.push(waveString);
+    }
+  }
+  return waveArray;
+}
--- js/kata8.js
+++ js/kata8.js
@@ -180,3 +180,17 @@ function feast(beast, dish) {
     }
     else{return false}
   }
+  /*Take an array and remove every second element from the array. Always keep the first element and start removing with the next element.
+
+Example:
+["Keep", "Remove", "Keep", "Remove", "Keep", ...] --> ["Keep", "Keep", "Keep", ...]
+
+None of the arrays will be empty, so you don't have to worry about that!*/
+//solution:
+function removeEveryOther(arr) {
+  const result = [];
+  for (let i = 0; i < arr.length; i += 2) {
+    result.push(arr[i]);
+  }
+  return result;
+}
