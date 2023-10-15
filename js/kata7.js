5c41b542ac87fad70e6582493e71c2720bbc1b53
Nathan Sanchez <NathanSanchezDev@outlook.com>
2023-10-15 10:12:38 -0400

    Sum up an array of integers and create a conditional of 'odd' 'even'

--- js/kata7.js
+++ js/kata7.js
@@ -82,3 +82,28 @@ function accum(s) {
   }
     return array.join('-');
 }
+
+/*
+Task:
+Given a list of integers, determine whether the sum of its elements is odd or even.
+
+Give your answer as a string matching "odd" or "even".
+
+If the input array is empty consider it as: [0] (array with a zero).
+
+Examples:
+Input: [0]
+Output: "even"
+
+Input: [0, 1, 4]
+Output: "odd"
+
+Input: [0, -1, -5]
+Output: "even"
+*/
+
+//Solution:
+function oddOrEven(array) {
+  const arraySum = array.reduce((a, b) => a + b, 0);
+  return arraySum % 2 === 0 ? "even" : "odd";
+}
