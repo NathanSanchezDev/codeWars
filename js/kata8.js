ec975033fcb31ecb5db0dfd58f9b2eac59209f4b
Nathan Sanchez <NathanSanchezDev@outlook.com>
2023-10-18 14:11:03 -0400

    identify multiples of a given number and put the multiples into an array and must not go above limit solution added

--- js/kata8.js
+++ js/kata8.js
@@ -285,3 +285,17 @@ Examples:
 //Solution:
 const opposite = number => {return -number;}
 //What it does is basically a simple integer conversion to either negative or positive.
+
+/*
+In this simple exercise, you will build a program that takes a value, integer , and returns a list of its multiples up to another value, limit . If limit is a multiple of integer, it should be included as well. There will only ever be positive integers passed into the function, not consisting of 0. The limit will always be higher than the base.
+
+For example, if the parameters passed are (2, 6), the function should return [2, 4, 6] as 2, 4, and 6 are the multiples of 2 up to 6.
+*/
+//Solution:
+function findMultiples(integer, limit) {
+  const multiples = [];
+  for (let i = integer; i <= limit; i += integer) {
+    multiples.push(i);
+  }
+  return multiples;
+}
