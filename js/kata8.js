df0b1e42da591d91d412264320202a33d2af69c2
Nathan Sanchez <NathanSanchezDev@outlook.com>
2023-10-22 14:01:59 -0400

    simple kata 8 return string with template literal.

--- js/kata8.js
+++ js/kata8.js
@@ -299,3 +299,11 @@ function findMultiples(integer, limit) {
   }
   return multiples;
 }
+
+/*
+Debugging sayHello function
+The starship Enterprise has run into some problem when creating a program to greet everyone as they come aboard. It is your job to fix the code and get the program working again!
+*/
+//Solution:
+const sayHello = (name)=>{
+  return `Hello, ${name}`}
