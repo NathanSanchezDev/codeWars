6e6d1566fe19d9728a945fa83765fa2e28f975ca
Nathan Sanchez <NathanSanchezDev@outlook.com>
2023-10-22 14:06:36 -0400

    create an object with languages and the word hello included in the string provided. and return depending on the language input into the function

--- js/kata8.js
+++ js/kata8.js
@@ -307,3 +307,57 @@ The starship Enterprise has run into some problem when creating a program to gre
 //Solution:
 const sayHello = (name)=>{
   return `Hello, ${name}`}
+
+ /*
+ Your start-up's BA has told marketing that your website has a large audience in Scandinavia and surrounding countries. Marketing thinks it would be great to welcome visitors to the site in their own language. Luckily you already use an API that detects the user's location, so this is an easy win.
+
+The Task
+Think of a way to store the languages as a database. The languages are listed below so you can copy and paste!
+Write a 'welcome' function that takes a parameter 'language', with a type String, and returns a greeting - if you have it in your database. It should default to English if the language is not in the database, or in the event of an invalid input.
+The Database
+[ ("english", "Welcome")
+, ("czech", "Vitejte")
+, ("danish", "Velkomst")
+, ("dutch", "Welkom")
+, ("estonian", "Tere tulemast")
+, ("finnish", "Tervetuloa")
+, ("flemish", "Welgekomen")
+, ("french", "Bienvenue")
+, ("german", "Willkommen")
+, ("irish", "Failte")
+, ("italian", "Benvenuto")
+, ("latvian", "Gaidits")
+, ("lithuanian", "Laukiamas")
+, ("polish", "Witamy")
+, ("spanish", "Bienvenido")
+, ("swedish", "Valkommen")
+, ("welsh", "Croeso")
+]
+ */ 
+  //solution:
+  const languageDatabase = {
+    "english": "Welcome",
+    "czech": "Vitejte",
+    "danish": "Velkomst",
+    "dutch": "Welkom",
+    "estonian": "Tere tulemast",
+    "finnish": "Tervetuloa",
+    "flemish": "Welgekomen",
+    "french": "Bienvenue",
+    "german": "Willkommen",
+    "irish": "Failte",
+    "italian": "Benvenuto",
+    "latvian": "Gaidits",
+    "lithuanian": "Laukiamas",
+    "polish": "Witamy",
+    "spanish": "Bienvenido",
+    "swedish": "Valkommen",
+    "welsh": "Croeso"
+  };
+  
+  function greet(language) {
+    if (languageDatabase.hasOwnProperty(language)) {
+      return languageDatabase[language];
+    }
+    return languageDatabase["english"];
+  }
