bdf877b7e00eb1a10fc82c54cf8bb0be2459f1f9
Nathan Sanchez <NathanSanchezDev@outlook.com>
2023-10-29 21:23:05 -0400

    return result of true if number is perfectly squared.

--- js/kata7.js
+++ js/kata7.js
@@ -136,3 +136,15 @@ const removeUrlAnchor = (url) => {
   const parts = url.split('#')
       return parts[0]
   }
+//Task
+// Given an integral number, determine if it's a square number:
+// In mathematics, a square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
+// The tests will always use some integral number, so don't worry about that in dynamic typed languages.
+  //solution:
+  function isSquare(n) {
+    if (n < 0) {
+      return false;
+    }
+    const squareRoot = Math.sqrt(n);
+    return Number.isInteger(squareRoot);
+  }
